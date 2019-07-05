5c5bf622d99da9902300ade38c0faafac3c3a533
Zarino Zappia <mail@zarino.co.uk>
2019-07-05 01:29:33 +0100

    [WIP] Callback-based DMX receiver / serial printer

--- dmx-receiver-arduino/dmx-receiver-arduino.ino
+++ dmx-receiver-arduino/dmx-receiver-arduino.ino
@@ -22,20 +22,51 @@ DMX_Slave dmx_slave(DMX_SLAVE_CHANNELS);
 
 SoftwareSerial software_serial(SERIAL_RX_PIN, SERIAL_TX_PIN);
 
+// We think there's some weird timing issue if you try to
+// software_serial.print() inside the onReceiveComplete callback.
+// So here we set up an array to batch up values, so we can print
+// a bunch of them at once, outside the getChannelValue() loop.
+#define BATCH_SIZE 20
+uint8_t valuesToPrint[BATCH_SIZE];
+uint8_t valuesStored = 0;
+
 void setup() {
     // Enable DMX slave interface and start recording DMX data.
     dmx_slave.enable();
 
     dmx_slave.setStartAddress(DMX_START_ADDRESS);
 
+    // Tell DMX_Slave to call this callback when it receives a full frame.
+    dmx_slave.onReceiveComplete(onFrameReceiveComplete);
+
     software_serial.begin(SERIAL_BAUD_RATE);
 }
 
 void loop() {
-    // Send the values of the DMX channels, over serial, as space separated string.
-    for (int i=DMX_START_ADDRESS; i<DMX_START_ADDRESS+DMX_SLAVE_CHANNELS; i++) {
-        software_serial.print( dmx_slave.getChannelValue(i) );
-        software_serial.print( ' ' );
+
+}
+
+void onFrameReceiveComplete (unsigned short channelsReceived) {
+    if ( channelsReceived == DMX_SLAVE_CHANNELS) {
+
+        for (int i=DMX_START_ADDRESS; i<DMX_START_ADDRESS+DMX_SLAVE_CHANNELS; i++) {
+            valuesToPrint[valuesStored] = dmx_slave.getChannelValue(i);
+            valuesStored++;
+        }
+
+        if (valuesStored == BATCH_SIZE) {
+
+            int8_t index = 0;
+            for (int row=0; row<(BATCH_SIZE / DMX_SLAVE_CHANNELS); row++) {
+                for (int j=0; j<DMX_SLAVE_CHANNELS; j++) {
+                    software_serial.print( valuesToPrint[ index++] );
+                    software_serial.print( " " );
+                }
+                software_serial.println();
+            }
+
+            valuesStored = 0;
+        }
+
     }
-    software_serial.println();
 }
