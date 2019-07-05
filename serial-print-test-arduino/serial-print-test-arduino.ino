4d3e62384c1951a26533f0da245a2c714c33a1e5
Zarino Zappia <mail@zarino.co.uk>
2019-07-05 18:54:28 +0100

    Improved Arduino software serial test

--- serial-print-test-arduino/serial-print-test-arduino.ino
+++ serial-print-test-arduino/serial-print-test-arduino.ino
@@ -1,15 +1,24 @@
-int i = 0;
+#include <SoftwareSerial.h>
+
+// We're using Software Serial, because the normal serial pins
+// and USB serial comms are being used by the DMX shield.
+#define SERIAL_RX_PIN 10
+#define SERIAL_TX_PIN 11
+#define SERIAL_BAUD_RATE 9600
+
+SoftwareSerial software_serial(SERIAL_RX_PIN, SERIAL_TX_PIN);
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
+    software_serial.begin(SERIAL_BAUD_RATE);
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  Serial.println(i);
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(400);
-  i++;
+    software_serial.print("255 255 255 255 255 ");
+    software_serial.println();
+
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(400);
 }
